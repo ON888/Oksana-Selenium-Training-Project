7f1e863ee566f854c0914d789b6b17808c99bfc1
Oksana Nahibina <oksana.nahibina@jll.com>
2023-06-13 17:35:43 -0700

    Added Home work with Login and WO flow tests

--- FirstSeleniumTest.cs
+++ FirstSeleniumTest.cs
@@ -7,9 +7,9 @@ namespace Oksana_Selenium_Training_Project
 {
     public class Tests
     {
-        IWebDriver drvChrome;
+        //IWebDriver drvChrome;
         IWebDriver drvFirefox;
-        IWebDriver drvEdge;
+        //IWebDriver drvEdge;
 
         [SetUp]
         public void Setup()
@@ -17,42 +17,42 @@ namespace Oksana_Selenium_Training_Project
             FirefoxOptions options = new FirefoxOptions();
             options.AcceptInsecureCertificates = true;
 
-            drvChrome = new ChromeDriver(); // create instance of the Chrome browser
+            //drvChrome = new ChromeDriver(); // create instance of the Chrome browser
             drvFirefox = new FirefoxDriver(options); // create instance of the Firefox browser
-            drvEdge = new EdgeDriver(); // create instance of the Edge browser
+            //drvEdge = new EdgeDriver(); // create instance of the Edge browser
         }
 
         [TearDown]
         public void TearDown() 
         { 
-            drvChrome.Quit(); //close browser
-            drvFirefox.Quit(); //close browser
-            drvEdge.Quit(); //close browser
+            //drvChrome.Quit(); //close browser
+            //drvFirefox.Quit(); //close browser
+            //drvEdge.Quit(); //close browser
         }
 
-        [Test]
-        public void Test_Chrome()
-        {
-            drvChrome.Navigate().GoToUrl("https://www.google.com/");
-            drvChrome.FindElement(By.CssSelector("[name=q]")).SendKeys("Selenium");
+        //[Test]
+        //public void Test_Chrome()
+        //{
+        //    drvChrome.Navigate().GoToUrl("https://www.google.com/");
+        //    drvChrome.FindElement(By.CssSelector("[name=q]")).SendKeys("Selenium"+ Keys.Enter);
 
-            //Assert.Pass();
-        }
+        //    //Assert.Pass();
+        //}
         [Test]
         public void Test_Firefox()
         {
             drvFirefox.Navigate().GoToUrl("https://www.google.com/");
-            drvFirefox.FindElement(By.CssSelector("[name=q]")).SendKeys("Selenium");
+            drvFirefox.FindElement(By.CssSelector("[name=q]")).SendKeys("Selenium" + Keys.Enter);
 
             //Assert.Pass();
         }
-        [Test]
-        public void Test_Edge()
-        {
-            drvEdge.Navigate().GoToUrl("https://www.google.com/");
-            drvEdge.FindElement(By.CssSelector("[name=q]")).SendKeys("Selenium");
+        //[Test]
+        //public void Test_Edge()
+        //{
+        //    drvEdge.Navigate().GoToUrl("https://www.google.com/");
+        //    drvEdge.FindElement(By.CssSelector("[name=q]")).SendKeys("Selenium"+ Keys.Enter);
 
-            //Assert.Pass();
-        }
+        //    //Assert.Pass();
+        //}
     }
 }
