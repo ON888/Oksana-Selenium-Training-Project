4a218ef1a9e86fb9fb1431b06f2f65d7b5bc8764
Oksana Nahibina <oksana.nahibina@jll.com>
2023-06-24 13:29:23 -0700

    Added waits to Login and WOPickUp

--- LoginTest/Login.cs
+++ LoginTest/Login.cs
@@ -1,19 +1,33 @@
+using DocumentFormat.OpenXml.VariantTypes;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace LoginTest
 {
-    public class Tests
+    public class Login
     {
         IWebDriver drv;
-        
+        WebDriverWait wait;
+
+        public string baseUrl = Environment.GetEnvironmentVariable("ENT_QA_BASE_URL");
+        public string password = Environment.GetEnvironmentVariable("ENT_QA_PASS");
+        public string username = Environment.GetEnvironmentVariable("ENT_QA_USER");
+        public string companyName = Environment.GetEnvironmentVariable("ENT_QA_COMPANY");
 
         [SetUp]
         public void Setup()
         {
             drv = new ChromeDriver(); // create instance of the Chrome browser
-            drv.Navigate().GoToUrl(Environment.GetEnvironmentVariable("ENT_QA_BASE_URL") +
-                    "/CorpNet/Login.aspx"); //Navigate to CE instance
+            //drv.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            wait = new WebDriverWait(drv, TimeSpan.FromSeconds(10));
+
+            drv.Navigate().GoToUrl(baseUrl + "/CorpNet/Login.aspx"); //Navigate to CE instance
+
+            Assert.True(isVisible(By.CssSelector(".login-page-header")));
+
         }
 
         [TearDown]
@@ -21,18 +35,59 @@ namespace LoginTest
         {
             drv.Quit(); //close browser
         }
+        private bool IsElementPresent(By locator)
+        {
+            return drv.FindElements(locator).Count > 0;
+        }
+
+        private bool isVisible(By locator)
+        {
+            return drv.FindElements(locator).Count > 0;
+        }
 
         [Test]
-        public void InvalidUsername()
+        public void isElementPresent ()
         {
-            //User verification
-            drv.FindElement(By.Id("username")).SendKeys("test");
-            drv.FindElement(By.Id("password")).SendKeys((Environment.GetEnvironmentVariable("ENT_QA_PASS")));
-            drv.FindElement(By.Name("_companyText")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
-            drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
-            Thread.Sleep(4000);
+            drv.Navigate().GoToUrl(baseUrl + "/corpnet/common/dashboard.aspx");
+            
+            if (IsElementPresent(By.Id("username")))// check if username filed is present on the page and then fill in data
+            {
+
+                drv.FindElement(By.Id("username")).SendKeys($"{username}"); //enter username from env variables
+                drv.FindElement(By.Id("password")).SendKeys($"{password}"); //enter password from env variables
+                drv.FindElement(By.Name("_companyText")).SendKeys($"{companyName}");
+                drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
+
+            }
+                wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop")));// wait uuser element is visible
+            
+            if (wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.Id("username"))))
+            {
+                drv.FindElement(By.CssSelector(".menu-secondary .menu-user")).Click();
+                drv.FindElement(By.CssSelector("li.menu-logout a")).Click();
+            }
+        }
+
+
+        [TestCase("test", "test", "QA Requestor Summer")]
+        //[TestCase($"{username}", "test", $"{company}")]
+        //[TestCase($"{username}", $"{company}", "test")]
+        public void InvalidCredentialsLogin(string username1, string password1, string company1)
+        {
+            //Invalid credentials provided; Error message received verification
+
+            bool loginPage = IsElementPresent(By.CssSelector(".login-page-header"));
+
+            if (loginPage)
+            {
+
+                drv.FindElement(By.Id("username")).SendKeys(username1);
+                drv.FindElement(By.Id("password")).SendKeys(password1);
+                drv.FindElement(By.Name("_companyText")).SendKeys(company1);
+                drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
+            }
+
             var error = drv.FindElement(By.CssSelector("div.login-table-b ul li")).Text;
-            var loginPage = drv.FindElement(By.CssSelector("#mainContentWrapper"));
 
             Assert.Multiple(() =>
             {
@@ -40,64 +95,29 @@ namespace LoginTest
                 Assert.That(loginPage, Is.True);
             });
         }
+        
         [Test]
-        public void InvalidPass()
-        {
-            //Password verification
-            drv.FindElement(By.Id("username")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER"));
-            drv.FindElement(By.Id("password")).SendKeys("test");
-            drv.FindElement(By.Name("_companyText")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
-            drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
-            Thread.Sleep(4000);
-            var error = drv.FindElement(By.CssSelector("div.login-table-b ul li")).Text;
-            var loginPage = drv.FindElement(By.CssSelector("#mainContentWrapper"));
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(error, Is.EqualTo("Invalid User ID or Password"));
-                Assert.That(loginPage, Is.True);
-            });
-        }
-
-        [Test]
-        public void InvalidCompany()
-        {
-            //Company verification
-            drv.FindElement(By.Id("username")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER"));
-            drv.FindElement(By.Name("_companyText")).SendKeys("test");
-            drv.FindElement(By.Id("password")).SendKeys((Environment.GetEnvironmentVariable("ENT_QA_PASS")));
-            drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
-            Thread.Sleep(4000);
-            var error = drv.FindElement(By.CssSelector("div.login-table-b ul li")).Text;
-            var loginPage = drv.FindElement(By.CssSelector("#mainContentWrapper"));
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(error, Is.EqualTo("Invalid User ID or Password"));
-                Assert.That(loginPage, Is.True);
-            });
-        }
-
-        [Test]
-        public void ValidTest()
+        public void ValidCredentialsLogin()
         {
             //Succesful Login User logged verification
 
-            drv.FindElement(By.Id("username")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER")); //enter username from env variables
-            drv.FindElement(By.Id("password")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_PASS")); //enter password from env variables
-            drv.FindElement(By.Name("_companyText"))
-            .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
-            drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
-            Thread.Sleep(4000);
+            bool loginPage = IsElementPresent(By.CssSelector(".login-page-header")); // 
 
-            var user = drv.FindElement(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop")).GetAttribute("text");        
-            var loginPage = drv.FindElement(By.CssSelector("#mainContentWrapper"));
-
+            if (loginPage) //check if login page header page is present
+            {
+                drv.FindElement(By.Id("username")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER")); //enter username from env variables
+                drv.FindElement(By.Id("password")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_PASS")); //enter password from env variables
+                drv.FindElement(By.Name("_companyText")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
+                drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
+            }
+            var user = drv.FindElement(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop"));        
+            
             Assert.Multiple(() =>
             {
-                Assert.That(user, Is.EqualTo("ENT_QA_USER"));
-                Assert.That(loginPage, Is.False);
+                Assert.That(isVisible(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop")), Is.True);
+                Assert.That(user.Text, Is.EqualTo("Quality Assurance"));
             });
         }
+        
     }
 }
--- WOPickUpTest/WOPickUpTest.cs
+++ WOPickUpTest/WOPickUpTest.cs
@@ -1,11 +1,18 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace WOPickUpTest
 {
     public class OksanaTests
     {
         IWebDriver drv;
+        WebDriverWait wait;
+        public string baseUrl = Environment.GetEnvironmentVariable("ENT_QA_BASE_URL");
+        public string password = Environment.GetEnvironmentVariable("ENT_QA_PASS");
+        public string username = Environment.GetEnvironmentVariable("ENT_QA_USER");
+        public string companyName = Environment.GetEnvironmentVariable("ENT_QA_COMPANY");
 
         [SetUp]
         public void Setup()
@@ -13,17 +20,20 @@ namespace WOPickUpTest
             //Login to CE instance using Environment variables
 
             drv = new ChromeDriver(); // create instance of the Chrome browser
-            drv.Navigate().GoToUrl(Environment.GetEnvironmentVariable("ENT_QA_BASE_URL") +
-                     "/CorpNet/Login.aspx"); //Navigate to CE instance
-            drv.FindElement(By.Id("username")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER")); //enter username from env variables
-            drv.FindElement(By.Id("password")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_PASS")); //enter password from env variables
-            drv.FindElement(By.Name("_companyText"))
-            .SendKeys(Environment.GetEnvironmentVariable("ENT_QA_COMPANY"));
+
+            wait = new WebDriverWait(drv, TimeSpan.FromSeconds(10));
+
+            drv.Navigate().GoToUrl(baseUrl + "/CorpNet/Login.aspx"); //Navigate to CE instance
+            //wait.Until(ExpectedConditions.ElementIsVisible(By.Name("#username")));
+
+            drv.FindElement(By.Id("username")).SendKeys($"{username}"); //enter username from env variables
+            drv.FindElement(By.Id("password")).SendKeys($"{password}"); //enter password from env variables
+            drv.FindElement(By.Name("_companyText")).SendKeys($"{companyName}");
             drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
-            Thread.Sleep(4000);
-            
-            Assert.True(isVisible(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop")));
-        }
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop")));// wait uuser element is visible
+
+        }    
 
         [TearDown]
         public void TearDown()
@@ -31,54 +41,61 @@ namespace WOPickUpTest
             drv.Quit(); //close browser
         }
 
-        [Test]
-        public void TestWOflow()
-        {
-            //Navigate to WO List page
-            drv.Navigate().GoToUrl(Environment.GetEnvironmentVariable("ENT_QA_BASE_URL") +
-                     "/CorpNet/workorder/workorderlist.aspx");
-            
-            // Preserve WO# with New Status that is going to be Picked Up to return to t later
-            var woNumber = drv.FindElement(By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a")).Text;
-
-            //Click on New WO and open Quick View dialog
-            drv.FindElement(By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a")).Click();
-
-            // Action Pick Up
-            drv.FindElement(By.XPath("//div[@data-role='woqvdialog']/div[@class='modal-body']/form/div/div/span[@title='Pick-Up']")).Click();
-
-            //Activate Comment area
-            drv.FindElement(By.XPath("//form[@class='corrigo-form']/div/textarea")).Click();
-
-            // Type a Comment
-            drv.FindElement(By.XPath("//form[@class='corrigo-form']/div/textarea")).SendKeys("Pick-Up Comment");
-
-            //Close WO Quick View dialog
-            drv.FindElement(By.XPath("//div[@data-role='woactionpickupeditdialog']/div/div/button[@class='btn btn-primary id-btn-save']")).Click();
-            
-            //Check in the Activity log that Action "Picked Up" is displayed
-            var action = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']/div[@class='k-grid k-widget k-reorderable']/table/tbody/tr/td[@data-column='ActionTitle'][contains(text(),'Picked Up')]"));
-            Assert.That(action.Text, Is.EqualTo("Picked Up"));
-
-            //Check in the Activity log that Comment is displayed
-            var comment = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']/div[@class='k-grid k-widget k-reorderable']/table/tbody/tr/td[@data-column='Comment'][contains(text(),'Pick-Up Comment')]"));
-            Assert.That(comment.Text, Is.EqualTo("Pick-Up Comment"));
-
-            //Closing the Quick View dialog
-            drv.FindElement(By.XPath("//button[@class='close btn-dismiss']")).Click();
-
-            //Check WO Staus is Opened on WO list page
-            var woStatus=drv.FindElement(By.XPath("//td[@data-column='Number']/a[contains(text(), "+ woNumber+ ")]/../../td[@class='gc-WoListGrid-WOStatus left type-grid_string '][contains(text(), 'Open')]"));
-
-            Assert.That(woStatus.Text, Is.EqualTo("Open"));
-
-
-            Thread.Sleep(4000);
-        }
-
         private bool isVisible(By locator)
         {
             return drv.FindElements(locator).Count > 0;
         }
+        private bool IsElementPresent(By locator)
+        {
+            return drv.FindElements(locator).Count > 0;
+        }
+
+        [Test]
+        public void TestWOflow()
+        {
+            //Navigate to WO List page
+            drv.Navigate().GoToUrl($"{baseUrl }" + "/CorpNet/workorder/workorderlist.aspx");
+            
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#WoListGrid")));
+            
+                // Preserve WO# with New Status that is going to be Picked Up to return to t later
+                var woNumber = drv.FindElement(By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a")).Text;
+                var commentText = "Pick-Up Comment Auto";
+                //Click on New WO and open Quick View dialog
+                drv.FindElement(By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a")).Click();
+
+                // Action Pick Up
+                drv.FindElement(By.XPath("//div[@data-role='woqvdialog']/div[@class='modal-body']//*[@title='Pick-Up']")).Click();
+
+                //Activate Comment area
+                drv.FindElement(By.XPath("//form[@class='corrigo-form']/div/textarea")).Click();
+
+                // Type a Comment
+                drv.FindElement(By.XPath("//form[@class='corrigo-form']/div/textarea")).SendKeys(commentText);
+
+                //Close WO Quick View dialog
+                drv.FindElement(By.XPath("//div[@data-role='woactionpickupeditdialog']//button[@class='btn btn-primary id-btn-save']")).Click();
+
+            
+            //Check in the Activity log that Action "Picked Up" is displayed
+            var action = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']//tr/td[@data-column='ActionTitle'][contains(text(),'Picked Up')]"));
+            Assert.That(action.Text, Is.EqualTo("Picked Up"));
+
+            //Check in the Activity log that Comment is displayed
+            var comment = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']//tr/td[@data-column='Comment'][contains(text(),'Pick-Up')]"));
+            Assert.That(comment.Text, Is.EqualTo(commentText));
+
+            //Closing the Quick View dialog
+            drv.FindElement(By.XPath("//button[@class='close btn-dismiss']")).Click();
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#WoListGrid")));
+
+            //Check WO Staus is Opened on WO list page
+            var woStatus = drv.FindElement(By.XPath("//td[@data-column='Number']/a[contains(text(), $'{woNumber}')]/../../td[@data-column='WOStatus']"));
+
+            Assert.That(woStatus.Text, Is.EqualTo("Open"));
+                       
+        }
+            
     }
 }
