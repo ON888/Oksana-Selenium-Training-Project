84029572582807099716c26066f1b9f8c6421ae0
Oksana Nahibina <oksana.nahibina@jll.com>
2023-06-25 13:09:31 -0700

    Added waits part 3

--- WOPickUpTest/WOPickUpTest.cs
+++ WOPickUpTest/WOPickUpTest.cs
@@ -9,10 +9,10 @@ namespace WOPickUpTest
     {
         IWebDriver drv;
         WebDriverWait wait;
-        public string baseUrl = Environment.GetEnvironmentVariable("ENT_QA_BASE_URL");
-        public string password = Environment.GetEnvironmentVariable("ENT_QA_PASS");
-        public string username = Environment.GetEnvironmentVariable("ENT_QA_USER");
-        public string companyName = Environment.GetEnvironmentVariable("ENT_QA_COMPANY");
+        private string baseUrl = Environment.GetEnvironmentVariable("ENT_QA_BASE_URL");
+        private string password = Environment.GetEnvironmentVariable("ENT_QA_PASS");
+        private string username = Environment.GetEnvironmentVariable("ENT_QA_USER");
+        private string company= Environment.GetEnvironmentVariable("ENT_QA_COMPANY");
 
         [SetUp]
         public void Setup()
@@ -21,14 +21,15 @@ namespace WOPickUpTest
 
             drv = new ChromeDriver(); // create instance of the Chrome browser
 
-            wait = new WebDriverWait(drv, TimeSpan.FromSeconds(10));
+            wait = new WebDriverWait(drv, TimeSpan.FromSeconds(5)); // explicit waits: wait object creation
 
-            drv.Navigate().GoToUrl(baseUrl + "/CorpNet/Login.aspx"); //Navigate to CE instance
-            //wait.Until(ExpectedConditions.ElementIsVisible(By.Name("#username")));
+            drv.Navigate().GoToUrl(baseUrl + "/CorpNet/Login.aspx"); //Navigate to CE instance.
+                                                                     //Selenium will automatically wait 
+                                                                     //page is loaded and next command will be executed
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("username"))).SendKeys($"{username}"); //wait and enter username from env variables
 
-            drv.FindElement(By.Id("username")).SendKeys($"{username}"); //enter username from env variables
             drv.FindElement(By.Id("password")).SendKeys($"{password}"); //enter password from env variables
-            drv.FindElement(By.Name("_companyText")).SendKeys($"{companyName}");
+            drv.FindElement(By.Name("_companyText")).SendKeys($"{company}");
             drv.FindElement(By.CssSelector("input.btn.login-submit-button")).Click(); //SendKeys(Keys.Enter);
 
             wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.menu-secondary ul li.menu-user a.menu-drop")));// wait uuser element is visible
@@ -41,34 +42,27 @@ namespace WOPickUpTest
             drv.Quit(); //close browser
         }
 
-        private bool isVisible(By locator)
-        {
-            return drv.FindElements(locator).Count > 0;
-        }
-        private bool IsElementPresent(By locator)
-        {
-            return drv.FindElements(locator).Count > 0;
-        }
-
         [Test]
         public void TestWOflow()
         {
             //Navigate to WO List page
             drv.Navigate().GoToUrl($"{baseUrl }" + "/CorpNet/workorder/workorderlist.aspx");
             
-            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#WoListGrid")));
-            
-                // Preserve WO# with New Status that is going to be Picked Up to return to t later
-                var woNumber = drv.FindElement(By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a")).Text;
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#WoListGrid")));// wait until grid is visible.
+
+            // Preserve WO# with New Status that is going to be Picked Up to return to t later
+                var wolink = By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a");
+                var woLinkElement = wait.Until(drv => drv.FindElement(wolink));
+                var woNumber = drv.FindElement(wolink).Text;
                 var commentText = "Pick-Up Comment Auto";
-                //Click on New WO and open Quick View dialog
+             //Click on New WO and open Quick View dialog
                 drv.FindElement(By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a")).Click();
 
-                // Action Pick Up
-                drv.FindElement(By.XPath("//div[@data-role='woqvdialog']/div[@class='modal-body']//*[@title='Pick-Up']")).Click();
+             // Action Pick Up with wait for element to be clickable
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[@data-role='woqvdialog']//span[@title='Pick-Up']"))).Click();
 
-                //Activate Comment area
-                drv.FindElement(By.XPath("//form[@class='corrigo-form']/div/textarea")).Click();
+            //Activate Comment area
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//form[@class='corrigo-form']/div/textarea"))).Click();
 
                 // Type a Comment
                 drv.FindElement(By.XPath("//form[@class='corrigo-form']/div/textarea")).SendKeys(commentText);
@@ -76,22 +70,29 @@ namespace WOPickUpTest
                 //Close WO Quick View dialog
                 drv.FindElement(By.XPath("//div[@data-role='woactionpickupeditdialog']//button[@class='btn btn-primary id-btn-save']")).Click();
 
-            
+
             //Check in the Activity log that Action "Picked Up" is displayed
-            var action = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']//tr/td[@data-column='ActionTitle'][contains(text(),'Picked Up')]"));
+            var table = drv.FindElement(By.CssSelector("#WoQvActivityLogGrid tbody"));
+            wait.Until(ExpectedConditions.StalenessOf(table));// wait untill table is stable/updated
+
+            var action = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']//tr[1]/td[@data-column='ActionTitle']"));
             Assert.That(action.Text, Is.EqualTo("Picked Up"));
 
             //Check in the Activity log that Comment is displayed
-            var comment = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']//tr/td[@data-column='Comment'][contains(text(),'Pick-Up')]"));
+            var comment = drv.FindElement(By.XPath("//div[@id='WoQvActivityLogGrid']//tr[1]/td[@data-column='Comment']"));
             Assert.That(comment.Text, Is.EqualTo(commentText));
 
             //Closing the Quick View dialog
             drv.FindElement(By.XPath("//button[@class='close btn-dismiss']")).Click();
 
-            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#WoListGrid")));
+            wait.Until(ExpectedConditions.StalenessOf(woLinkElement));
+
+            //wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#WoListGrid")));
+            //wait.Until(ExpectedConditions.StalenessOf(drv.FindElement(By.CssSelector("#WoListGrid tbody"))));
+            //wait.Until(drv => drv.FindElement(By.CssSelector("#WoListGrid")));
 
             //Check WO Staus is Opened on WO list page
-            var woStatus = drv.FindElement(By.XPath("//td[@data-column='Number']/a[contains(text(), $'{woNumber}')]/../../td[@data-column='WOStatus']"));
+            var woStatus = drv.FindElement(By.XPath("//td[@data-column='Number']/a[contains(text(), '" + woNumber +"')]/../../td[@data-column='WOStatus']"));
 
             Assert.That(woStatus.Text, Is.EqualTo("Open"));
                        
